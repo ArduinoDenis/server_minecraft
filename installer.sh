f02a1e325e7cb48b0104ac7a19c99b36e8511d14
ArduinoDenis <90153974+ArduinoDenis@users.noreply.github.com>
2022-09-28 15:32:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,13 +4,13 @@ sudo apt-get install default-jdk -y ;
 javac -version;
 mkdir minecraft;
 sudo chmod 777 minecraft/;
-wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz;
-tar -xvf OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz;
-sudo rm -rf OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz;
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.4.1%2B1/OpenJDK17U-jdk_x64_linux_hotspot_17.0.4.1_1.tar.gz;
+tar -xvf OpenJDK17U-jdk_x64_linux_hotspot_17.0.4.1_1.tar.gz;
+sudo rm -rf OpenJDK17U-jdk_x64_linux_hotspot_17.0.4.1_1.tar.gz;
 sudo cp start.sh minecraft/
 cd minecraft/
 sudo chmod 777 *;
-wget https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar
+wget https://download.getbukkit.org/spigot/spigot-1.19.2.jar
 chmod 777 *
 sudo apt-get update && sudo apt-get upgrade -y;
 sudo add-apt-repository ppa:linuxuprising/java -y;
