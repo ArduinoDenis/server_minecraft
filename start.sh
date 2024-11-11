8772c20a720b3d0511ee7665d5de0399cfcf479e
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-11-11 19:07:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BINDIR=$(dirname "$(readlink -fn "$0")")
 cd "$BINDIR"
-java -Xmx2048M -Xms2048M -jar spigot-1.20.4.jar -o true
+java -Xmx2048M -Xms2048M -jar paper-1.21.1-131.jar -o true
